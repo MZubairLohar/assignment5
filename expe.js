2bef1fd63b018f5f802c38a2359827ac48b8b946
MZubairLohar <zubairgituser@gmail.com>
2019-11-06 09:59:37 -0800

    experiment file first task of assignment

---
+++ expe.js
@@ -0,0 +1,24 @@
+/*
+var cleanetCities = ["karachi", "hydrabad", "lahore","islamabad","faislabad" ]
+for (var i=0; i<5; i++){
+        if(cleanetCities = [i]){
+        alert("It's one of the cleanest cities");
+}
+else {
+        alert("no it's not one of cleanest cities");
+}
+}
+*/
+
+var num = 5;
+
+for (i = num ; i >= 1 ; i--){
+        
+
+       num = i-1;
+}
+console.log(num);
+      
+
+
+
