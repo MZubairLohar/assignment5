d4db649e0076ee8b116208ff4ea63b18918203ad
MZubairLohar <zubairgituser@gmail.com>
2019-11-10 21:21:00 +0500

    experiment.

--- expe.js
+++ expe.js
@@ -1,24 +1,53 @@
-/*
-var cleanetCities = ["karachi", "hydrabad", "lahore","islamabad","faislabad" ]
-for (var i=0; i<5; i++){
-        if(cleanetCities = [i]){
-        alert("It's one of the cleanest cities");
+
+//var number = (Math.floor(Math.random()*100+1))
+/*var number = 5;
+alert("Number guessing game")
+var guess1 = prompt("type your first guessed number");
+if( guess1 == number ){
+        alert ("You are right, You won! ")
+        break;
+}else {
+        alert("sorry, Incorrect Please try again , You have 2 more chances");
 }
-else {
-        alert("no it's not one of cleanest cities");
+
+
+
+
+
+
+var guess2 = prompt("type your second guessed number");
+
+if( guess2 == number ){
+        alert ("You are right, You won! ")
+}else {
+        alert("sorry, Incorrect Please try again , You have 1 more chances");
 }
+var guess3 = prompt("type your first guessed number");
+if( guess3 === number ){
+        alert ("You are right, You won! ")
+}else {
+        alert("sorry, Incorrect Please try again , You lost your chances");
 }
+var chance = prompt("wanna Try again, then type 'yes' if dont please type 'No'");
+if( Chance === "yes"){
+        return number()
+}
+
+
 */
 
-var num = 5;
 
-for (i = num ; i >= 1 ; i--){
+
+alert("number");
+var answer = 3;
+var guess = prompt("type number");
+for(i=0 ; i <2 ; i++ ){
+        if(answer == guess){
+                alert("correct");
+                break; 
+        }else {
+                guess = prompt("try again");
+         
         
-
-       num = i-1;
-}
-console.log(num);
-      
-
-
-
+        } }
+       
