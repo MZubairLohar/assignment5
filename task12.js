a6ac609a2205992e08d2080b834509a53e8f4674
MZubairLohar <zubairgituser@gmail.com>
2019-11-14 22:49:12 +0500

    update task12 from zubair

--- task12.js
+++ task12.js
@@ -1,1 +1,39 @@
+var b =(Math.floor(Math.random)*100+1);
+var a = ("Ready go")
+alert ("Game Starts now ")
+
+function gusNum() {
+var gusNu = prompt("type your number");}
+gusNum();
+function gameWon(){
+    if ( gusNum == b){
+        alert("You Won");
+        return gusNum;}
+    }
+
+function gameLost1(){
+    if(gusNum != b){
+        alert( "You Lost");
+        alert( "two more chances");
+        
+
+        alert("Try Again" + document.querySelector("p").innerHTML)
+    }
+}
+function looping(){
+    if(gameLost1()){
+        for(i=0;i<3;i++){
+        return gusNum();}
+        
+    }
+}
+
+    gameWon();
+        
+        gameLost1();
+        looping();
+  
+
+
+
 
